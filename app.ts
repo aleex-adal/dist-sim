f0899199ac5048f90f94db41d92601cc9d22515d
aleex-adal <aleex.adal@gmail.com>
2019-12-06 16:50:33 -0500

    m

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ import express = require('express');
 const app: express.Application = express();
 
 app.get('/', function (req, res) {
-  nodeTest(res); 
+  nodeTest(res);
 });
 
 app.listen(3000, function () {
@@ -12,9 +12,23 @@ app.listen(3000, function () {
 });
 
 function nodeTest(res: any) {
-    let nodeArr = [];
-    for (let i=0; i < 20; i++) {
-        nodeArr[i] = new node(i);
-    }
-    res.send(nodeArr);
+  let nodeArr: node[] = [];
+  let connectArr: number[] = [];
+
+  for (let i=0; i < 20; i++) {
+    connectArr.push(i);
+  }
+
+  for (let i=0; i < 20; i++) {
+      let n = new node(i);
+      console.log(connectArr);
+      let index = Math.round(Math.random() * connectArr.length);
+      console.log(index); // figure out why this is throwing nulls
+
+      let c = connectArr.splice(index, 1)[0];
+      n.connections.push(c);
+      nodeArr.push(n);
+  }
+
+  res.send(nodeArr);
 }
