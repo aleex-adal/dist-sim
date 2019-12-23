81a0d9f4a511e5eb3892e7f8c92b9ac7b3b9c6f5
aleex-adal <sopranalto@gmail.com>
2019-12-23 18:16:37 -0500

    frontend config

--- api/app.ts
+++ api/app.ts
@@ -17,42 +17,42 @@ app.listen(4000, async () => {
   let net: network = new network(10);
   let n: node = net.getNode(8);
 
+  // console.log(
+  //   await n.read(36)
+  // );
+
+  // n.update(36, { fruit: 'this update should be delayed' }, 5000)
+  // .then( (val: any) => {
+  //   console.log('{\n    item:  ' + JSON.stringify(val.item) + '\n    dataNode\'s clock:  ' + val.sourceClock + '\n    requestNode\'s clock:  ' + val.requestClock + '\n    msg:  ' + val.msg + '\n}');
+  // });
+
+  // n.update(36, { fruit: 'YOLO_SWAG', extraField: 'This is an extra field!'})
+  // .then( (val: any) => {
+  //   console.log('{\n    item:  ' + JSON.stringify(val.item) + '\n    dataNode\'s clock:  ' + val.sourceClock + '\n    requestNode\'s clock:  ' + val.requestClock + '\n    msg:  ' + val.msg + '\n}');
+  // });
+
+
   console.log(
-    await n.read(36)
+   await n.insert({ fruit: 'I just inserted this fruit yo'})
   );
 
-  n.update(36, { fruit: 'this update should be delayed' }, 5000)
-  .then( (val: any) => {
-    console.log('{\n    item:  ' + JSON.stringify(val.item) + '\n    dataNode\'s clock:  ' + val.sourceClock + '\n    requestNode\'s clock:  ' + val.requestClock + '\n    msg:  ' + val.msg + '\n}');
-  });
+  console.log(
+    await n.insert({ fruit: 'I just inserted this fruit AGAIN yo'})
+  );
 
-  n.update(36, { fruit: 'YOLO_SWAG', extraField: 'This is an extra field!'})
-  .then( (val: any) => {
-    console.log('{\n    item:  ' + JSON.stringify(val.item) + '\n    dataNode\'s clock:  ' + val.sourceClock + '\n    requestNode\'s clock:  ' + val.requestClock + '\n    msg:  ' + val.msg + '\n}');
-  });
+  console.log(
+    await n.insert({ fruit: '3'})
+  );
 
+  console.log(
+    await n.insert({ fruit: '4'})
+  );
 
-  // console.log(
-  //  await n.insert({ fruit: 'I just inserted this fruit yo'})
-  // );
+  console.log(
+    await n.insert({ fruit: '5'})
+  );
 
-  // console.log(
-  //   await n.insert({ fruit: 'I just inserted this fruit AGAIN yo'})
-  // );
-
-  // console.log(
-  //   await n.insert({ fruit: '3'})
-  // );
-
-  // console.log(
-  //   await n.insert({ fruit: '4'})
-  // );
-
-  // console.log(
-  //   await n.insert({ fruit: '5'})
-  // );
-
-  // console.log(
-  //   await n.insert({ fruit: '6'})
-  // );
+  console.log(
+    await n.insert({ fruit: '6'})
+  );
 });
