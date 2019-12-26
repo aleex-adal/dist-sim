02441c5210acddca56f59b3095e16df19c4772c7
aleex-adal <sopranalto@gmail.com>
2019-12-25 22:51:42 -0500

    blink

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import menu from './resource/menu.svg';
 
 function App() {
+  const [menuClasses, setMenuClasses] = useState(['menu', 'overlay', 'before-textarea blink']);
+
+  useEffect(() => {
+    document.documentElement.style.setProperty('--prompt-width', document.getElementById("prompt").offsetWidth + 'px');
+    console.log('prompt width: ' + document.getElementById("prompt").offsetWidth);
+  }, []);
+
   return (
     <div className="app">
       <header>
@@ -10,17 +17,19 @@ function App() {
           <div className="nav-title">
             dist-sim
           </div>
-          <img className="menu-btn" src={menu} alt="Menu icon"></img>
+          <img className="menu-btn" src={menu} alt="Menu icon" onClick={() => setMenuClasses(['menu menu-active', 'overlay overlay-active'])}></img>
         </nav>
       </header>
       <div className="sim">
-        Hello World!
       </div>
       <div className="console">
-        >>> Here is some sample text
+        <div id="prompt" className={menuClasses[2]}>>>></div>
+        <textarea onClick={() => {console.log('calling onclick'); const newMenuClasses = menuClasses; newMenuClasses[2] = 'before-textarea'; setMenuClasses(newMenuClasses)}}></textarea>
       </div>
 
-      <div className="menu">
+      <div className={menuClasses[1]} onClick={() => setMenuClasses(['menu', 'overlay'])}></div>
+
+      <div className={menuClasses[0]}>
         <ul>
           <li>about</li>
         </ul>
