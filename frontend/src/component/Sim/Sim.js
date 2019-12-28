31b19821db669997d055346a32507f064b4f9a04
aleex-adal <sopranalto@gmail.com>
2019-12-28 16:32:41 -0500

    connect

--- frontend/src/component/Sim/Sim.js
+++ frontend/src/component/Sim/Sim.js
@@ -54,15 +54,30 @@ const Sim = (props) => {
 
 			const deg = (360 / num) * i;
 
+			var width = (document.getElementById("circle-wrapper").offsetWidth / 2) + 'px';
+
 			document.getElementById('circle-wrapper').append(newDiv);
-			document.getElementById(i).style.transform = 'rotate(-90deg) rotate(' + deg + 'deg) translate(150px) rotate(-' + deg + 'deg) rotate(90deg)';
+			document.getElementById(i).style.transform = "rotate(-90deg) rotate(" + deg + "deg) translate(" + width + ") rotate(-" + deg + "deg) rotate(90deg)";
+			document.getElementById(i).style.lineHeight = document.getElementById(i).offsetWidth - 2 + 'px'; // reset line height, - 2 is border width
 		}
+
+		// test, connect 0 with 3
+		var cxZero = document.getElementById('0').getBoundingClientRect().left;
+
+		var subtractBy = document.getElementById('0').getBoundingClientRect().top;
+		var cyZero = document.getElementById('0').getBoundingClientRect().top - subtractBy;
+
+		var cxThree = document.getElementById('3').getBoundingClientRect().left;
+		var cyThree = document.getElementById('3').getBoundingClientRect().top - subtractBy;
+
+		console.log('0: ' + cxZero + ' ' + cyZero + ', 3: ' + cxThree + ' ' + cyThree);
 	};
 
 	return (
   	<div id="sim-wrapper" className="sim-wrapper">
       <div id="circle-wrapper" className="circle-wrapper">
-			
+	  	<svg width="500" height="500"><line x1="179.71875" y1="0" x2="351.38446044921875" y2="236.277587890625" stroke="black"/></svg>
+
       </div>
     </div>
 	)
